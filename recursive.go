9f2adcc3fb81add62f184846a1a84d17ef00684c
Fathan Akbar <37922280+fathnakbar@users.noreply.github.com>
2024-12-23 16:02:10 +0700

    Update recursive.go, nanoseconds

--- recursive.go
+++ recursive.go
@@ -61,5 +61,5 @@ func main() {
             fmt.Printf("Region: %s, Demand: %d\n", region.Region, region.Demand)
         }
     
-        fmt.Printf("Execution Time: %s\n", duration)
+        fmt.Printf("Execution Time: %s\n", duration. Nanoseconds())
 }
