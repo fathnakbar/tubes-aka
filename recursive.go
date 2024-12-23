3f50a28474bca0d100a19110728d89a9e0866d05
Muhammad Fathan <fathnakbar@gmail.com>
2024-12-23 13:25:37 +0700

    fix(recursive): add runtime measurement

--- recursive.go
+++ recursive.go
@@ -45,11 +45,18 @@ func main() {
         {"Medan", 1400},
     }
 
-    // Pengurutan
-    sortedData := quickSortRecursive(data)
+        // Mengukur waktu eksekusi
+        startTime := time.Now()
 
-    // Output hasil
-    for _, region := range sortedData {
-        fmt.Printf("Region: %s, Demand: %d\n", region.Region, region.Demand)
-    }
+        // Pengurutan
+        sortedData := quickSortRecursive(data)
+    
+        duration := time.Since(startTime)
+    
+        // Output hasil
+        for _, region := range sortedData {
+            fmt.Printf("Region: %s, Demand: %d\n", region.Region, region.Demand)
+        }
+    
+        fmt.Printf("Execution Time: %s\n", duration)
 }
